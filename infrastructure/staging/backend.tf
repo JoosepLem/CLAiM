e4051e7ea5a665aa8a847bb39767caed3aef25f9
Kevin Keskyla <kevinkeskyla@gmail.com>
2026-07-08 12:38:24 +0300

    Fix invalid use_lockfile backend arg and create plugin cache dir before init

--- infrastructure/staging/backend.tf
+++ infrastructure/staging/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket       = "claim-staging-tfstate-257526643965"
-    key          = "staging/terraform.tfstate"
-    region       = "eu-north-1"
-    encrypt      = true
-    use_lockfile = true
+    bucket  = "claim-staging-tfstate-257526643965"
+    key     = "staging/terraform.tfstate"
+    region  = "eu-north-1"
+    encrypt = true
   }
 }
